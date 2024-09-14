ddeeb0b7dcb12a751f8aee10c94ca2c17e0528eb
cristianpb <felipebrokate@gmail.com>
2024-09-14 15:42:35 +0200

    🎨 Add cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,34 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+		'./src/**/*.{html,js,svelte,ts}',
+  ],
+
+  // This is the function used to extract class names from your templates
+  defaultExtractor: content => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+    return broadMatches.concat(innerMatches)
+  }
+})
+
 
 const config = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
-		autoprefixer,
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    process.env.NODE_ENV === 'production' ? autoprefixer : null,
+    ...(process.env.NODE_ENV === 'production' ? [cssnano({ preset: 'default' })] : []),
+    //...process.env.NODE_ENV === 'production' ? [purgecss] : [],
 	]
 };
 

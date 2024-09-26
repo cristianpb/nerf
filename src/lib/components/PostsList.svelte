29da614556c47ef2a00ca1c4af74b060dce5655e
cristianpb <felipebrokate@gmail.com>
2024-09-26 21:30:55 +0200

    💄Fix space between cards in blog

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -5,11 +5,11 @@
 </script>
 
 
-<div class="space-y-4 grid md:grid-cols-2">
-	{#each posts as post}
-  <Card img="{base}{post.coverImage}" href="{base}/blog/{post.slug}" horizontal size="md" >
+<div class="space-y-4 md:space-y-0 grid md:grid-cols-2">
+  {#each posts as post}
+    <Card img="{base}{post.coverImage}" href="{base}/blog/{post.slug}" horizontal size="md" >
     <h5 class="mb-2 text-2xl text-justify font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
     <p class="mb-3 font-normal text-justify text-gray-700 dark:text-gray-400 leading-tight">{post.excerpt}</p>
-  </Card>
-	{/each}
+    </Card>
+  {/each}
 </div>
